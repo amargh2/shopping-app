e69c49eabb3503546ff3bef7aaeaf0ad1d92b289
amargh2 <margherioa@gmail.com>
2023-01-31 12:38:43 -0600

    added a button to the empty cart state, fixed header behavior on cart page

--- src/components/CartComponents/Cart.js
+++ src/components/CartComponents/Cart.js
@@ -3,12 +3,19 @@ import Header from '../PageComponents/Header'
 import NavBar from '../PageComponents/NavBar'
 import MakeCartItemCards from './makeCartItemCards'
 import TotalDisplay from './TotalDisplay'
-
+import { Link } from 'react-router-dom'
 const Cart = (props) => {
   //checks the cart -- if empty, tells the user to go add items to their cart
     function chooseOutput() {
       if (props.total === 0) {
-        return (<div className = 'flex justify-center py-10 font-medium'>Your cart is currently empty! Visit the shop and add some items to your cart!</div>)
+        return (<div className = 'flex flex-col items-center gap-2 justify-center p-10 font-medium'>Your cart is currently empty! Visit the shop and add some items to your cart!
+                <Link to='/shop'>
+                  <button
+                    className="rounded-md bg-cyan-400 px-3.5 py-1.5 text-base font-semibold leading-7 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                      Go to the shop
+                  </button>
+              </Link>
+                </div>)
       } if (props.total !== 0) {
         return(
         <div className='grid sm:grid-cols-2 lg:grid-cols-10 py-10 px-2'>
@@ -39,7 +46,10 @@ const Cart = (props) => {
   
   return (
       <div>
-        <NavBar cartItemCount={props.cartItemCount}/>
+      <div className='justify-center lg:hidden sm:flex'>
+        <Header></Header>
+      </div>        
+      <NavBar cartItemCount={props.cartItemCount}/>
           <div>
               <div>{chooseOutput()}</div>
         
--- src/components/PageComponents/HomePage.js
+++ src/components/PageComponents/HomePage.js
@@ -4,28 +4,6 @@ import NavBar from "./NavBar";
 import Image from './homepagepic.jpg'
 import {GiCrystalGrowth} from 'react'
 import { Link } from "react-router-dom";
-/*const HomePage = (props) => {
-  return (
-    <div>
-      <Header/>
-      <NavBar cartItemCount={props.cartItemCount}/>
-        <div class=''>
-          <div className="mx-auto sm:px-6 lg:px-8 flex">
-            <div className="bg-white shadow rounded-lg flex">
-                <div className = 'flex justify-center'>
-                  <img src='images/home.jpg' className = 'w-2/3 h-1/2 rounded-lg' alt="hello" />
-                    <div className="px-4 py-5 sm:p-6 shadow-lg">
-                      <p className = 'font-semibold text-2xl'> Welcome to the Crystal Shop! We have a great selection of beautiful, resonant crystals that are sure to meet your spiritual and aesthetic needs!</p>
-                    </div>
-                  </div>
-                </div>
-            </div>
-          </div>
-        </div>  
-  )
-}
-
-export default HomePage */
 
 export default function HomePage(props) {
   return (
